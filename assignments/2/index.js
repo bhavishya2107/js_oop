f33bb2ceb6970b8b71b3e28c9865022e5c8a9746
Bhavishya <bhavishya2107@gmail.com>
2019-12-10 17:42:27 +0530

    commit index.js and main.js

--- assignments/2/index.js
+++ assignments/2/index.js
@@ -6,16 +6,18 @@
 
 function makePerson(name, age) {
 	// add code here
-
-
+var obj = {}
+obj.name = name
+obj.age = age
+return obj
 }
 
-var vicky = makePerson('Vicky', 24);
+var vicky = makePerson('Vicky', 29);
 
 
 // /********* Uncomment these lines to test your work! *********/
-// console.log(vicky.name); // -> Logs 'Vicky'
-// console.log(vicky.age); // -> Logs 24
+console.log(vicky.name); // -> Logs 'Vicky'
+console.log(vicky.age); // -> Logs 24
 
 
 
@@ -29,9 +31,14 @@ var vicky = makePerson('Vicky', 24);
 
 var personStore = {
 	// add code here
-
+  greet: function() {
+    console.log('hello')
+  }
 
 };
+
+var hello = Object.create(personStore)
+hello.greet()
 
 // /********* Uncomment this line to test your work! *********/
 // personStore.greet(); // -> Logs 'hello'
@@ -39,28 +46,35 @@ var personStore = {
 
 
 /*** CHALLENGE 2 of 3 ***/
+// Create a function `personFromPersonStore` that takes as input a `name` and an `age`. When called, the function will create person objects using the `Object.create` method on the `personStore` object.
 
 function personFromPersonStore(name, age) {
-	// add code here
-
-
+  // add code here
+  var hello = Object.create(personStore)
+  hello.name = name
+  hello.age = age
+  return hello
 }
 
 var sandra = personFromPersonStore('Sandra', 26);
 
 
-// /********* Uncomment these lines to test your work! *********/
-// console.log(sandra.name); // -> Logs 'Sandra'
-// console.log(sandra.age); //-> Logs 26
-// sandra.greet(); //-> Logs 'hello'
 
+
+// /********* Uncomment these lines to test your work! *********/
+console.log(sandra.name); // -> Logs 'Sandra'
+console.log(sandra.age); //-> Logs 26
+// sandra.greet(); //-> Logs 'hello'
 
 
 /*** CHALLENGE 3 of 3 ***/
 
 // add code here
+personStore.introduce = function(){
+  console.log(`Hi, my name is ${this.name}`)
+}
 
-// sandra.introduce(); // -> Logs 'Hi, my name is Sandra'
+sandra.introduce(); // -> Logs 'Hi, my name is Sandra'
 
 
 
@@ -71,43 +85,51 @@ var sandra = personFromPersonStore('Sandra', 26);
 ****************************************************************/
 
 /*** CHALLENGE 1 of 3 ***/
-
+// Create a function `PersonConstructor` that uses the `this` keyword to save a single property onto its scope called `greet`. `greet` should be a function that logs the string 'hello'.
 function PersonConstructor() {
 	// add code here
-
-
+  this.greet = function(){
+    console.log('hello')
+  }
 }
+
 
 
 // /********* Uncomment this line to test your work! *********/
 var simon = new PersonConstructor;
-// simon.greet(); // -> Logs 'hello'
+simon.greet(); // -> Logs 'hello'
 
 
 
 /*** CHALLENGE 2 of 3 ***/
+// Create a function `personFromConstructor` that takes as input a `name` and an `age`. When called, the function will create person objects using the `new` keyword instead of the Object.create method.
 
 function personFromConstructor(name, age) {
-	// add code here
-
-
+  // add code here
+  var person = new PersonConstructor(name, age);
+  person.name = name;
+  person.age = age;
+  return person;
 }
 
 var mike = personFromConstructor('Mike', 30);
 
 
 // /********* Uncomment these lines to test your work! *********/
-// console.log(mike.name); // -> Logs 'Mike'
-// console.log(mike.age); //-> Logs 30
+console.log(mike.name); // -> Logs 'Mike'
+console.log(mike.age); //-> Logs 30
 // mike.greet(); //-> Logs 'hello'
 
 
 
 /*** CHALLENGE 3 of 3 ***/
 // add code here
+PersonConstructor.prototype.introduce = function() {
+  console.log(`Hi, my name is ${this.name}` )
+}
 
 
-// mike.introduce(); // -> Logs 'Hi, my name is Mike'
+mike.introduce(); // -> Logs 'Hi, my name is Mike'
 
 
 /****************************************************************
@@ -117,32 +139,40 @@ var mike = personFromConstructor('Mike', 30);
 /*** CHALLENGE 1 of 3 ***/
 
 class PersonClass {
-	constructor() {
+	constructor(name) {
     // add code here
-
+    this.name = name
 
 	}
 
-	// add code here
+  // add code here
+  greet() {
+    console.log('hello')
+  }
 
 }
 
 
 // /********* Uncomment this line to test your work! *********/
 var george = new PersonClass;
-// george.greet(); // -> Logs 'hello'
+george.greet(); // -> Logs 'hello'
 
 
 
 /*** CHALLENGE 2 of 3 ***/
 
 // add code here
+class DeveloperClass extends PersonClass {
+  introduce() {
+    console.log(`Hello World, my name is ${this.name}`)
+  }
+}
 
 
 // /********* Uncomment these lines to test your work! *********/
-// var thai = new DeveloperClass('Thai', 32);
-// console.log(thai.name); // -> Logs 'Thai'
-// thai.introduce(); //-> Logs 'Hello World, my name is Thai'
+var thai = new DeveloperClass('Thai', 32);
+console.log(thai.name); // -> Logs 'Thai'
+thai.introduce(); //-> Logs 'Hello World, my name is Thai'
 
 
 
@@ -164,16 +194,25 @@ function userFactory(name, score) {
   return user;
 }
 
-var adminFunctionStore /* Put code here */ ;
+var adminFunctionStore = Object.create(userFunctionStore);
 
 function adminFactory(name, score) {
   // Put code here
+  var obj = new userFactory(name, score)
+  obj.type = "Admin"
+  return obj
 }
 
 /* Put code here for a method called sharePublicMessage*/
+adminFactory().__proto__.sharePublicMessage = function() {
+  console.log('Welcome Users')
+}
 
 var adminFromFactory = adminFactory("Eva", 5);
 
+
 // /********* Uncomment these lines to test your work! *********/
-// adminFromFactory.sayType() // -> Logs "I am a Admin"
-// adminFromFactory.sharePublicMessage() // -> Logs "Welcome users!"
+adminFromFactory.sayType() // -> Logs "I am a Admin"
+adminFromFactory.sharePublicMessage()
+// userFactory.sharePublicMessage()
+ // -> Logs "Welcome users!"
---
+++ assignments/3. class/main.js
@@ -0,0 +1,40 @@
+class BookList {
+    constructor() {
+
+    }
+
+    addBook(books) {
+        if(books = []){
+    
+        }
+    }
+
+    finishCurrentBook() {
+
+    }
+
+    
+ 
+
+}
+
+
+
+class Book {
+
+    constructor(title,genre,author) {
+        this.title = title
+        this.genre = genre
+        this.author = author
+        this.read = false
+        this.readDate = new Date(Date.now())
+    }
+
+}
+
+function BookList () {
+    var bookRead = new Book;
+    bookRead.read = true
+}
+
+
